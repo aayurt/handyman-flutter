d546eeb18b913b30dd29782ab6949f36d8566eb9
Gurung-Manish <manishgrg496@gmail.com>
2023-08-30 18:05:26 +0100

    all till now

--- lib/features/profile/presentation/pages/profile_page.dart
+++ lib/features/profile/presentation/pages/profile_page.dart
@@ -38,7 +38,6 @@ class _ProfilePageState extends State<ProfilePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
       body: SafeArea(
           child: Column(children: [
         Stack(
